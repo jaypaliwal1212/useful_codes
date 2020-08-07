bcba569b409711b39514a481137a481ea4cd77c6
Jay Paliwal <50298189+jaypaliwal1212@users.noreply.github.com>
2020-08-07 07:56:22 +0530

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,6 +1,9 @@
+"""This is a simple code which allows user to select a .b64 file, decode it and write the data obtained into a pdf"""
+
+
 import base64
-f = open("/home/jay_paliwal/Downloads/Encoded_File.b64","r")
-c = open("/home/jay_paliwal/Downloads/file.pdf","wb") 
+f = open("/path/Encoded_File.b64","r")
+c = open("/path/output_file.pdf","wb") 
 text = base64.b64decode(f.read(),validate=True)
 f.close()
 c.write(text)
